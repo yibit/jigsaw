1bee28af4b5790509b64eabdcb9fcd68556fb719
陈旭 <chen.xu8@zte.com.cn>
2018-11-15 23:16:53 +0800

    ...

--- simple-html-parser.js
+++ simple-html-parser.js
@@ -1,36 +1,60 @@
 
 var input= `
-<ul>
-  <li *ngFor="let i of [1,2,3,4,5]">
-  {{this.dataBus.text}}:  {{i}}
-  </li>
-    
-<span>{{this.dataBus.text}}</span>
-<span>{{this.dataBus.text123.bdfd}}</span>
-
-  <hr *ngFor="let i of [1,2,3,4,5]" style="padding: 8px">
-<span>{{this.dataBus.text}}</span>
-<span>{{this.dataBus.text123.bdfd}}</span>
+<ul style="width:100px;">
+ <li></li>
 </ul>
 `
 
-var pos = 0;
-while(true) {
-    var tagStart = matchWithIndex(input, /<.+?\s+/, pos);
-    var tagStartPos = tagStart ? tagStart.index : input.length;
-    var quoteStart = matchWithIndex(input, /"|'/, pos);
-    var quoteStartPos = quoteStart ? quoteStart.index : input.length;
+var max = 100;
+var nodes = [];
+var pendingTag = [];
 
-    if (tagStartPos < quoteStartPos) {
-        findTagEnd(input, tagStartPos + tagStart[0].length);
-    } else if (quoteStartPos < tagStartPos) {
+    debugger;
+findNextNode(input, 0);
+console.log(nodes);
 
-    } else {
-        break;
+
+
+
+function findNextNode(input, from) {
+    for (var i = from; i < input.length; i++) {
+        if (input[i] == '<') {
+            if (input[i+1] == '/') {
+                // closing tag
+            } else {
+                // a new child tag
+                findNode(input, i);
+            }
+            return i;
+        } else if (input[i].match(/"|'/)) {
+            i = findQuoteEnd(input, i + 1, input[i]);
+        }
     }
 
+
+
+    // input = input.substring(from);
+    // var tagStart = input.match(/<.+?\s+|>/);
+    // var tagStartPos = tagStart ? tagStart.index : input.length;
+    // var quoteStart = input.match(/"|'/);
+    // var quoteStartPos = quoteStart ? quoteStart.index : input.length;
+
+    // if (tagStartPos < quoteStartPos) {
+    //     findNode(input, tagStartPos, tagStart[0]);
+    // } else if (quoteStartPos < tagStartPos) {
+    //     var quoteInfo = findQuote(input, quoteStartPos);
+    //     if (quoteInfo) {
+    //         findNextNode(input, quote.index);
+    //     }
+    // } else {
+    //     // the end
+    // }
+    // return tagStart;
 }
 
+function isClosingTag(input, from) {
+    input.substring(from).match(/<\/.+?\s*>/);
+}
 
 function findTagEnd(input, from) {
     var tagEnd = input.length;
@@ -39,9 +63,38 @@ function findTagEnd(input, from) {
             tagEnd = i;
             break;
         }
-        if (input[i].match(/"|'/)) {
-            i = findQuoteEnd(input, i + 1);
+    }
+    return tagEnd;
+}
+
+function findNode(input, from) {
+    var sub = input.substring(from);
+    var match = sub.match(/<(.+?)\s/);
+    var tagName = match ? match[1] : sub;
+    var node = new Node(tagName);
+    pendingTag.push(node);
+
+    var start = from + tagName.length;
+    while(true) {
+        var attr = findAttribute(input, start);
+        if (!attr) {
+            break;
         }
+        node.attributes.push(attr);
+        start = attr.endIndex + 1;
+    }
+
+    var lastAttr = node.attributes[node.attributes.length - 1];
+    var idx = findTagEnd(input, lastAttr.endIndex);
+    if (idx !== input.length) {
+        var nextTagStart = findNextNode(input, idx + 1);
+        node.innerHtml = input.substring(from, nextTagStart);
+        pendingTag.pop(node);
+        if (pendingTag.length == 0) {
+            nodes.push(node);
+        }
+    } else {
+        node.innerHtml = input.substring(from);
     }
 
 }
@@ -51,15 +104,14 @@ function findAttribute(input, from) {
     for (var i = nameEndIndex; i < input.length; i++) {
         if (input[i] == '=') {
             var name = input.slice(from, i);
-            var quoteStart = findQuoteStart(input, i);
-            var endIndex = quoteStart ? findQuoteEnd(input, quoteStart.index, quoteStart.quote) : i;
+            var quoteStart = findQuoteStart(input, i+1);
+            var endIndex = quoteStart ? findQuoteEnd(input, quoteStart.index + 1, quoteStart.quote) : i;
             var value = quoteStart ? input.slice(i + 1, endIndex) : undefined;
             return {name: name, value: value, startIndex: from, endIndex: endIndex};
         } else if (input[i] == '>') {
-            var name = input.slice(from, i);
+            var name = input.slice(from, i).trim();
             var value = undefined;
-            var endIndex = i;
-            return {name: name, value: value, startIndex: from, endIndex: endIndex};
+            return name ? {name: name, value: value, startIndex: from, endIndex: endIndex} : null;
         }
     }
 
@@ -76,13 +128,12 @@ function findAttributeNameEnd(input, from) {
 
 function findQuoteStart(input, from) {
     for (var i = from; i < input.length; i++) {
-        if (input[i] == '>') {
-            return null;
-        }
-        if (input[i].match(/"|'/)) {
-            return {index: i, quote: input[i]};
+        var match = input[i].match(/"|'/);
+        if (match) {
+            return {index:i, quote: match[0]};
         }
     }
+    return null;
 }
 
 function findQuoteEnd(input, from, quote) {
